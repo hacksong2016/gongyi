78b090df8d17b68699ea815f55c51109f6f6d0ff
puw <puw@unicall.cc>
2016-07-27 04:06:11 +0800

    addhead

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import {createApp} from 'mantra-core';
 import initContext from './configs/context';
-import {DocHead} from 'meteor/kadira:dochead';
 
 // modules
 import coreModule from './modules/core';
@@ -8,8 +7,6 @@ import searchModule from './modules/search';
 
 // init context
 const context = initContext();
-DocHead.addMeta({name: "viewport", content: "width=device-width, initial-scale=1"});
-DocHead.addMeta({rel: "shortcut icon", content: "/images/favicon.ico"});
 
 // create app
 const app = createApp(context);
